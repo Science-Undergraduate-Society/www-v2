7dcb3763af2ff1f544dd46b4845eda937b3bb82f
dvidEast <daviddlim11@gmail.com>
2025-07-16 16:28:15 -0700

    (scaffolding) cleaned up public files, changed favicon to sus, removed next boilerplate from home page

--- frontend/src/data/routes.ts
+++ frontend/src/data/routes.ts
@@ -1,189 +1,20 @@
-// Search Bar uses title and content to query through
-const searchPages = [
+// All pages / links used for Navbar
+const pageRoutes = [
     {
-        title: "Apply",
-        href: "/joinSus/apply",
-        content: "Information about applying to SUS.",
+        title: "Home",
+        href: "/",
+        content: "Mission + History, Calendar"
     },
     {
-        title: "Spring Elections 2024",
-        href: "/joinSus/elections",
-        content: "Information about the SUS Spring ELections 2024.",
+        title: "Council",
+        href: "/aboutSus/council",
+        content: "What it is, goals, who attends."
     },
     {
-        title: "Budget",
-        href: "/aboutUs/budget",
-        content: "Learn more about our organization's budget.",
-    },
-    {
-        title: "Clubs",
-        href: "/aboutUs/clubs",
-        content: "Learn more about our organization's clubs.",
-    },
-    {
-        title: "Code and Procedures",
-        href: "/aboutUs/codeProcedures",
-        content: "Learn more about our organization's code and procedures.",
-    },
-    {
-        title: "Executives",
-        href: "/aboutUs/executives",
-        content: "Learn more about our organization's executives.",
-    },
-    {
-        title: "Meeting Minutes",
-        href: "/aboutUs/meetingMinutes",
-        content: "Learn more about the meeting minutes from our organisation.",
-    },
-    {
-        title: "Office Hours",
-        href: "/aboutUs/officeHours",
-        content: "Learn about the office hours our organisation holds.",
-    },
-    {
-        title: "Upcoming Events",
-        href: "/ourEvents/upcoming",
-        content: "Details about our upcoming events.",
-    },
-    {
-        title: "Past Events",
-        href: "/ourEvents/past",
-        content: "Details about our past events.",
-    },
-
-    {
-        title: "Blue Card Program",
-        href: "/studentServices/bluecard",
-        content: "Information about the Blue Card Program.",
-    },
-    {
-        title: "Grants",
-        href: "/studentServices/grants",
-        content: "Grant application and information.",
-    },
-    {
-        title: "Subsidies",
-        href: "/studentServices/grants",
-        content: "Information about the Blue Card Program.",
-    },
-    {
-        title: "Health & Wellness",
-        href: "/studentServices/health",
-        content: "Health and wellness resources.",
-    },
-    {
-        title: "Volunteer Portal",
-        href: "/studentServices/volunteerPortal",
-        content: "Portal for finding volunteer opportunities.",
-    },
-    {
-        title: "Study Sphere",
-        href: "/studentServices/studySphere",
-        content: "Information on our new notes sharing platform.",
-    },
-    {
-        title: "ALSSC",
-        href: "/scienceStudentCentre/aboutAlssc",
-        content: "Information about the Science Student Centre.",
-    },
-    {
-        title: "Book a Meeting Room",
-        href: "/scienceStudentCentre/meetingBooking",
-        content: "How to book a room at ALSSC.",
-    },
-    {
-        title: "Event Booking",
-        href: "/scienceStudentCentre/eventBooking",
-        content: "How to book an event at ALSSC.",
-    },
-    {
-        title: "Feedback Form",
-        href: "https://docs.google.com/forms/d/e/1FAIpQLSeoybfzo-4VZgMUgie-eySFPczi_ToTwugPDu8F-IPLoS04Wg/viewform",
-        content: "Give us feedback.",
-    },
-    {
-        title: "Shop",
-        href: "/shop",
-        content:
-        "Visit our shop for merchandise. Merch such as hoodies and crewnecks. Hoodie. Crewneck",
-    },
-    {
-        title: "Hoodie",
-        href: "/shop",
-        content:
-        "Visit our shop for merchandise. Merch such as hoodies and crewnecks. Hoodie. Crewneck",
-    },
-    {
-        title: "Crewneck",
-        href: "/shop",
-        content:
-        "Visit our shop for merchandise. Merch such as hoodies and crewnecks. Hoodie. Crewneck",
-    },
-    {
-        title: "Abdul Ladha",
-        href: "/scienceStudentCentre/abdulLadha",
-        content: "about the abdul ladha",
-    },
-    {
-        title: "Vancouver/BC Services & Resources",
-        href: "/studentServices/health",
-        content: "Vancouver/BC Services & Resources",
-    },
-    {
-        title: "Physical Wellness",
-        href: "/studentServices/health",
-        content: "Physical Wellness",
-    },
-    {
-        title: "Nutrition",
-        href: "/studentServices/health",
-        content: "Nutrition",
-    },
-    {
-        title: "Mental Wellness",
-        href: "/studentServices/health",
-        content: "Mental Wellness",
-    },
-    {
-        title: "Support For Indigenous People",
-        href: "/studentServices/health",
-        content: "Support For Indigenous People",
-    },
-    {
-        title: "Support For 2SLGBTQ+ People",
-        href: "/studentServices/health",
-        content: "Support For 2SLGBTQ+ People",
-    },
-    {
-        title: "Free Recreation",
-        href: "/studentServices/health",
-        content: "Free Recreation",
-    },
-    {
-        title: "Science RXN",
-        href: "/ourEvents/upcoming",
-        content: "Science RXN",
-    },
-    {
-        title: "Science Week",
-        href: "/ourEvents/upcoming",
-        content: "Science Week",
-    },
-    {
-        title: "Science Graduation",
-        href: "/ourEvents/upcoming",
-        content: "Science Graduation",
-    },
-    {
-        title: "Welcome Back BBQ",
-        href: "/ourEvents/past",
-        content: "Welcome Back BBQ",
-    },
-    {
-        title: "Student Recognition Awards Night",
-        href: "/ourEvents/past",
-        content: "Student Recognition Awards Night",
+        title: "Procedures",
+        href: "/aboutSus/procedures",
+        content: "Budget, code + procedures, by-laws"
     },
 ];
 
-export default searchPages;
+export default pageRoutes
