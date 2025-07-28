05fef2826304ad5d7e42f0131f2e6e91058d96c7
dvidEast <daviddlim11@gmail.com>
2025-07-28 10:13:44 -0700

    (footer) created scaffolding for component

---
+++ frontend/src/components/layout/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import { Suspense } from 'react'
+import styles from './Footer.module.css'
+
+export default function Footer({
+
+} : {
+
+}) {
+
+    return (
+        <div className={styles.container}>
+            Footer Component
+        </div>
+    )
+}
