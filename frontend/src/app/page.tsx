5df46cb02ff3a6e958e1dc65442b131c06d8284a
owenfisher <owenfisher46@gmail.com>
2025-07-30 13:43:22 -0700

    (Footer Component) Backlog Item - Implemented the Footer Component and added the necessary icons

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      Home page
+        <p>hello world</p>
     </div>
   );
 }
