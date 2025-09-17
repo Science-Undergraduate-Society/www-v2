adca9688d7b04f8ebb055dea0c256e92c054f7c2
alan-w-u <alanwu0004@gmail.com>
2025-09-17 10:31:01 -0700

    (Meeting Minutes) Meeting Minutes Mobile - update mobile support

--- frontend/src/components/features/WorkingGroupMeetingMinutes/WorkingGroupMeetingMinutes.tsx
+++ frontend/src/components/features/WorkingGroupMeetingMinutes/WorkingGroupMeetingMinutes.tsx
@@ -7,10 +7,11 @@ import styles from "./WorkingGroupMeetingMinutes.module.css";
 
 export default function WorkingGroupMeetingMinutes() {
     const [selected, setSelected] = useState(Object.keys(workingGroupMeetingMinutes)[0])
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false)
 
     return (
         <div className={styles.workingGroupMeetingMinutes}>
-            <ul>
+            <ul className={styles.meetingMinutesTabs}>
                 {Object.keys(workingGroupMeetingMinutes).map(workingGroup => (
                     <li
                         key={workingGroup}
@@ -21,12 +22,39 @@ export default function WorkingGroupMeetingMinutes() {
                     </li>
                 ))}
             </ul>
+            <div className={styles.meetingMinutesDropdown}>
+                <button
+                    className={styles.meetingMinutesDropdownButton}
+                    onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                >
+                    {selected}
+                    <div className={`${styles.meetingMinutesDropdownIcon} ${isDropdownOpen ? styles.open : ''}`} />
+                </button>
+                {isDropdownOpen && (
+                    <ul className={styles.meetingMinutesDropdownPanel}>
+                        {Object.keys(workingGroupMeetingMinutes)
+                            .filter(workingGroup => workingGroup !== selected)
+                            .map(workingGroup => (
+                            <li
+                                key={workingGroup}
+                                className={selected === workingGroup ? styles.selected : ''}
+                                onClick={() => {
+                                    setSelected(workingGroup)
+                                    setIsDropdownOpen(false)
+                                }}
+                            >
+                                {workingGroup}
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
             <div className={styles.meetingMinutesView}>
                 {workingGroupMeetingMinutes[selected]?.map(({ title, link }, index) => (
                     <GlassButton
                         key={index}
                         href={link}
-                        className={styles.button}
+                        className={styles.meetingMinutesButton}
                     >
                         {title}
                     </GlassButton>
