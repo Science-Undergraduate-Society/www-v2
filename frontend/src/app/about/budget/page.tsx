e1d9b3b1035ac3a6bc9a8f33d93bd2d41b17e098
Eojin Lee <142281569+elee131@users.noreply.github.com>
2025-11-05 08:34:15 -0800

    (Budget) Budget - create page

---
+++ frontend/src/app/about/budget/page.tsx
@@ -0,0 +1,14 @@
+import styles from './budget.module.css'
+
+export default function Budget({
+
+} : {
+
+}) {
+
+    return (
+        <div className={styles.container}>
+            Budget page
+        </div>
+    )
+}
