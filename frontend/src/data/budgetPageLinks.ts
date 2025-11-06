0613a2c74c6a6d212d28615ceffb1c7cb7ef0662
Eojin Lee <142281569+elee131@users.noreply.github.com>
2025-11-05 20:01:02 -0800

    (Budget) - Budget - add data file for glass buttons

---
+++ frontend/src/data/budgetPageLinks.ts
@@ -0,0 +1,26 @@
+export const glassButtonLinks = [
+        {
+            name: "Budget Overview '24/'25",
+            link: "https://drive.google.com/file/d/1qGn0VRMykNaHZi2ISfwJfx_bftQr3OXN/view"
+        }, 
+        {
+            name: "Semi-annual Budget Changes '24/'25",
+            link: "https://drive.google.com/file/d/1m9kDYcPFfXetmu2rkol_H2BWtLix0PcY/view"
+        }, 
+        {
+            name: "Budget Presentation '24/'25",
+            link: "https://drive.google.com/drive/folders/1SJYSaMw0CyQRvq2aU-vOFAesdk3cdaW8"
+        },
+        {
+            name: "Operational Surplus Request '24/'25",
+            link: "https://drive.google.com/file/d/1gL7NNiNntsW-ks-ul1FB9QNSRknCPuhC/view"
+        }, 
+        {
+            name: "Semi-annual Spending Summary '24/'25",
+            link: "https://drive.google.com/file/d/1qGn0VRMykNaHZi2ISfwJfx_bftQr3OXN/view"
+        }, 
+        {
+            name: "AGM Budget Updates '24/'25",
+            link: "https://drive.google.com/file/d/1XYdHZ77qgEBapMH3RTNnyPMc75X-TVE_/view"
+        }, 
+    ]
