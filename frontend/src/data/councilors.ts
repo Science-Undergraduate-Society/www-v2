7d74475943c973f79cd72bcea66a029247b579be
alan-w-u <alanwu0004@gmail.com>
2026-02-23 15:44:03 -0800

    upload councilor data

---
+++ frontend/src/data/councilors.ts
@@ -0,0 +1,230 @@
+import { Councilor } from "@/lib/types";
+
+export const executives: Councilor[] = [
+  {
+    name: "Rachel Dong",
+    role: "President",
+    email: "president@sus.ubc.ca",
+    image: "/assets/executive-images/executive-images/rachel-dong.png"
+  },
+  {
+    name: "Anika Lee",
+    role: "VP Academic",
+    email: "vpacademic@sus.ubc.ca",
+    image: "/assets/executive-images/executive-images/anika-lee.png"
+  },
+  {
+    name: "Alan Wei",
+    role: "VP Administration",
+    email: "vpadministration@sus.ubc.ca",
+    image: "/assets/executive-images/executive-images/alan-wei.jpg"
+  },
+  {
+    name: "Carmen Choo",
+    role: "VP Communications",
+    email: "vpcommunications@sus.ubc.ca",
+    image: "/assets/executive-images/executive-images/carmen-choo.png"
+  },
+  {
+    name: "Catherine Jiang",
+    role: "VP External",
+    email: "vpexternal@sus.ubc.ca",
+    image: "/assets/executive-images/executive-images/catherine-jiang.jpg"
+  },
+  {
+    name: "Judy Kim",
+    role: "VP Finance",
+    email: "vpfinance@sus.ubc.ca"
+  },
+  {
+    name: "Jenevieve Lee",
+    role: "VP Student Life",
+    email: "vpstudentlife@sus.ubc.ca",
+    image: "/assets/executive-images/executive-images/jenevieve-lee.png"
+  }
+]
+
+export const speaker: Councilor[] = [
+  {
+    name: "Conrad Chiu",
+    role: "Speaker"
+  }
+]
+
+export const clerks: Councilor[] = [
+  {
+    name: "Alyssa Wong",
+    role: "Associate Vice President, Administration",
+    email: "avp.administration@sus.ubc.ca"
+  },
+  {
+    name: "Gloria Zhuo",
+    role: "Associate Vice President, Administration",
+    email: "avp.administration@sus.ubc.ca"
+  }
+]
+
+export const amsRepresentativesAndSenator: Councilor[] = [
+  {
+    name: "Bardia Mohammadizadeh",
+    role: "AMS Representative",
+    email: "amsrep@sus.ubc.ca"
+  },
+  {
+    name: "Yolanda Yang",
+    role: "AMS Representative",
+    email: "amsrep@sus.ubc.ca"
+  },
+  {
+    name: "Abby Sun",
+    role: "AMS Representative",
+    email: "amsrep@sus.ubc.ca"
+  },
+  {
+    name: "Alex Chui",
+    role: "Science Student Senator",
+    email: "senator@sus.ubc.ca"
+  },
+]
+
+export const departmentRepresentatives: Councilor[] = [
+  {
+    name: "Janet Zhao",
+    role: "First Year Rep"
+  },
+  {
+    name: "Daniel Tong",
+    role: "First Year Rep"
+  },
+  {
+    name: "Rachel Kim",
+    role: "Biochemistry Rep"
+  },
+  {
+    name: "Muntaqim Syed",
+    role: "Biotechnology Rep"
+  },
+  {
+    name: "James Wu",
+    role: "CAPS Rep"
+  },
+  {
+    name: "Amir Teimour Zadeh",
+    role: "Chemistry Rep"
+  },
+  {
+    name: "Cindy Wu",
+    role: "Computer Science Rep"
+  },
+  {
+    name: "Allan Yao",
+    role: "Intergrated Science Rep"
+  },
+  {
+    name: "Sophia Park",
+    role: "Microbiology & Immunology Rep"
+  },
+  {
+    name: "Simren Ahuja",
+    role: "Neuroscience Rep"
+  },
+  {
+    name: "Anita Tang ",
+    role: "Pharmacology Rep"
+  },
+  {
+    name: "Benjamin Bradley",
+    role: "Physics & Astronomy Rep"
+  },
+  {
+    name: "Rain Zhong",
+    role: "Science One Rep"
+  },
+  {
+    name: "Yusuke Takami",
+    role: "Vantage Rep"
+  }
+]
+
+export const clubRepresentatives: Councilor[] = [
+  {
+    name: "Vlad Grecu",
+    role: "Astronomy Club"
+  },
+  {
+    name: "Amira Kadri",
+    role: "Bachelors of Computer Science (BCS) Student Association"
+  },
+  {
+    name: "Madeleine Huson",
+    role: "Biochemistry Student Association (BSA)"
+  },
+  {
+    name: "Alyssa Kunkel",
+    role: "Biological Sciences Society (BIOSOC)"
+  },
+  {
+    name: "Annie Wang",
+    role: "CAPS Student Association (CAPSSA)"
+  },
+  {
+    name: "Emily Chen",
+    role: "Cognitive Systems Society (CSS)"
+  },
+  {
+    name: "Cindy Wu",
+    role: "Computer Science Student Society (CSSS)"
+  },
+  {
+    name: "Carolina Sousa",
+    role: "Combined Major in Science Student Association (CMSSA)"
+  },
+  {
+    name: "Ishika Devjani",
+    role: "Data Science Club @ UBC"
+  },
+  {
+    name: "Mina Lander",
+    role: "Environmental Sciences Student Association (ESSA)"
+  },
+  {
+    name: "Keiran Hoang",
+    role: "Geography Student Association (GSA)"
+  },
+  {
+    name: "Alan Deng",
+    role: "Integrated Sciences Student Association (ISSA)"
+  },
+  {
+    name: "Sophia Park",
+    role: "Microbiology & Immunology Student Association (MISA)"
+  },
+  {
+    name: "Anita Tang ",
+    role: "Pharmacology Student Association (PSA)"
+  },
+  {
+    name: "Benjamin Bradley",
+    role: "Physics Society (PHYSSOC)"
+  },
+  {
+    name: "Rain Zhong",
+    role: "Science One Survivors (SOS)"
+  },
+  {
+    name: "Sophie Thanasack",
+    role: "UBC-BCIT Biotechnology Club"
+  },
+  {
+    name: "Chelsea Kim",
+    role: "UBC Neuroscience Association"
+  },
+  {
+    name: "Amir Zadeh ",
+    role: "Undergraduate Chemistry Society (UCS)"
+  },
+  {
+    name: "Vanya  Sharma",
+    role: "Undergraduate Statistics Society (USS)"
+  }
+]
