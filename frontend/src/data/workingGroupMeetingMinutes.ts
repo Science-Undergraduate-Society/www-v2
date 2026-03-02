32230a15664e60a38d14f6c0c0a405f9875a5fb5
dvidEast <daviddlim11@gmail.com>
2026-03-02 13:15:49 -0800

    update meeting minutes + clean up UI

--- frontend/src/data/workingGroupMeetingMinutes.ts
+++ frontend/src/data/workingGroupMeetingMinutes.ts
@@ -1,55 +1,55 @@
 import { WorkingGroupMeetingMinutes } from "@/lib/types";
 
 export const workingGroupMeetingMinutes: WorkingGroupMeetingMinutes = {
-    council: [
-        { title: 'Academic Affairs', link: 'https://drive.google.com/drive/folders/13s-xCcU-f-IgGdQk5a1qzQ3oTTJvE46M' },
-        { title: 'External Relations', link: 'https://drive.google.com/drive/folders/1URaRYfN1WSK5Ljm1hawfLOW8xGKYgUrP' },
-        { title: 'Joint Policy and Advocacy', link: 'https://drive.google.com/drive/folders/1jCeDAEESwhReqX9MwUMJrNp8HYiRVPTI' },
-        { title: 'President\'s Advisory' , link: 'https://drive.google.com/drive/folders/12cRLHp0SYPIcyGRr3jJNIl35ls4-pYUQ' },
-        { title: 'Student Life and Events', link: 'https://drive.google.com/drive/folders/1w6mksIxf9dzr0FKfWfAIi_FUgt9kbMUk' }
+    Council: [
+        { title: 'Academic Affairs', link: 'https://drive.google.com/drive/folders/13s-xCcU-f-IgGdQk5a1qzQ3oTTJvE46M?usp=drive_link' },
+        { title: 'External Relations', link: 'https://drive.google.com/drive/folders/1URaRYfN1WSK5Ljm1hawfLOW8xGKYgUrP?usp=drive_link' },
+        { title: 'Joint Policy and Advocacy', link: 'https://drive.google.com/drive/folders/1jCeDAEESwhReqX9MwUMJrNp8HYiRVPTI?usp=drive_link' },
+        { title: 'President\'s Advisory' , link: 'https://drive.google.com/drive/folders/12cRLHp0SYPIcyGRr3jJNIl35ls4-pYUQ?usp=drive_link' },
+        { title: 'Student Life and Events', link: 'https://drive.google.com/drive/folders/1w6mksIxf9dzr0FKfWfAIi_FUgt9kbMUk?usp=drive_link' }
     ],
-    presidential: [
-        { title: 'Advocacy', link: 'https://drive.google.com/drive/folders/1NWWoe0UXD0zE-mXYW-AfSBlT6vtf75dy' },
-        { title: 'Human Resources (HR)', link: 'https://drive.google.com/drive/folders/1kxPtQSeZ0BNpae69Oah4S_gx9huksmnB' },
-        { title: 'Elections', link: 'https://drive.google.com/drive/folders/13mhYgFKqPjShS8grRed0U6ISzUJ_hvRj' },
-        { title: 'Equity, Diversity, and Inclusion (EDI)', link: 'https://drive.google.com/drive/folders/1caFd8f5YL6OLw_Dhf0j6Xezit13UdaSu' }
+    Presidential: [
+        { title: 'Advocacy', link: 'https://drive.google.com/drive/folders/1NWWoe0UXD0zE-mXYW-AfSBlT6vtf75dy?usp=drive_link' },
+        { title: 'Human Resources (HR)', link: 'https://drive.google.com/drive/folders/1kxPtQSeZ0BNpae69Oah4S_gx9huksmnB?usp=drive_link' },
+        { title: 'Elections', link: 'https://drive.google.com/drive/folders/13mhYgFKqPjShS8grRed0U6ISzUJ_hvRj?usp=drive_link' },
+        { title: 'Equity, Diversity, and Inclusion (EDI)', link: 'https://drive.google.com/drive/folders/1caFd8f5YL6OLw_Dhf0j6Xezit13UdaSu?usp=drive_link' }
     ],
-    administration: [
-        { title: 'Clubs (CC)', link: 'https://drive.google.com/drive/folders/1Cs9fzS754LZcplRK4TFI8o29YF7zPFMM' },
-        { title: 'Sustainability (SUST)', link: 'https://drive.google.com/drive/folders/1xue3ghZBw9cMighT5Xwv6Vhg-x6cLwyk' },
-        { title: 'Building Management (BMC)', link: 'https://drive.google.com/drive/folders/12t--jSzaJFEIWl2tFx_7uimrErb6N7vl' },
-        { title: 'Code and Policy (CAPC)', link: 'https://drive.google.com/drive/folders/1A2MMlncfqT9rwXkrekflQuH2wL2707um' }
+    Administration: [
+        { title: 'Clubs (CC)', link: 'https://drive.google.com/drive/folders/1Cs9fzS754LZcplRK4TFI8o29YF7zPFMM?usp=drive_link' },
+        { title: 'Sustainability (SUST)', link: 'https://drive.google.com/drive/folders/1xue3ghZBw9cMighT5Xwv6Vhg-x6cLwyk?usp=drive_link' },
+        { title: 'Building Management (BMC)', link: 'https://drive.google.com/drive/folders/12t--jSzaJFEIWl2tFx_7uimrErb6N7vl?usp=drive_link' },
+        { title: 'Code and Policy (CAPC)', link: 'https://drive.google.com/drive/folders/1A2MMlncfqT9rwXkrekflQuH2wL2707um?usp=drive_link' }
     ],
-    academic: [
-        { title: 'Wellness (WELL)', link: 'https://drive.google.com/drive/folders/1BQPuxGW_VskKu9jKnfX0YaMonWF-S-KB' },
-        { title: 'Academic Experience (ACAX)', link: 'https://drive.google.com/drive/folders/1MP8v-wb172vjBglYsgR7tnprOBlH6y0z' },
-        { title: 'Tutoring (T)', link: 'https://drive.google.com/drive/folders/1f6yToHgYx9p3h5ebMaX89ikKAa-BaIjW' },
-        { title: 'Mentorship (MENT)', link: 'https://drive.google.com/drive/folders/1FR3NqZ1vrHoWdmMF4nn4zwy9kHHJVgtG' }
+    Academic: [
+        { title: 'Wellness (WELL)', link: 'https://drive.google.com/drive/folders/1BQPuxGW_VskKu9jKnfX0YaMonWF-S-KB?usp=drive_link' },
+        { title: 'Academic Experience (ACAX)', link: 'https://drive.google.com/drive/folders/1MP8v-wb172vjBglYsgR7tnprOBlH6y0z?usp=drive_link' },
+        { title: 'Tutoring (T)', link: 'https://drive.google.com/drive/folders/1f6yToHgYx9p3h5ebMaX89ikKAa-BaIjW?usp=drive_link' },
+        { title: 'Mentorship (MENT)', link: 'https://drive.google.com/drive/folders/1FR3NqZ1vrHoWdmMF4nn4zwy9kHHJVgtG?usp=drive_link' }
     ],
-    communications: [
-        { title: 'First Week (FW)', link: 'https://drive.google.com/drive/folders/1C6D100bM86sqWnR8ZNe_ofpZxsZt0ZIe' },
-        { title: 'Marketing (MKTG)', link: 'https://drive.google.com/drive/folders/1H6e0B1qx5Qmrcyq8yt1uUYdfsVDabxRz' },
-        { title: 'Productions (PRODS)', link: 'https://drive.google.com/drive/folders/19xEJrWYceRueUfb2tMZoiX23WYG07McC' },
-        { title: 'Sales (SALE)', link: 'https://drive.google.com/drive/folders/1LcplWcstEOvq0Anli69ILa-s1VU77COZ' },
-        { title: 'Web Developer (WEB)', link: 'https://drive.google.com/drive/folders/1BI2v-tmlZPgS-K9B0fmFKT0DckdFNE5k' },
-        { title: 'Hackathon (HACK)', link: 'https://drive.google.com/drive/folders/1G5d6RKz5-xy5BTQ-fHDGbbh-cA0tof43' },
-        { title: 'Monthly Chair Meetings', link: 'https://drive.google.com/drive/folders/1p2W5XkpUDJdc0rHwdfJDbu0HD0y1ehdG' }
+    Communications: [
+        { title: 'First Week (FW)', link: 'https://drive.google.com/drive/folders/1C6D100bM86sqWnR8ZNe_ofpZxsZt0ZIe?usp=drive_link' },
+        { title: 'Marketing (MKTG)', link: 'https://drive.google.com/drive/folders/1H6e0B1qx5Qmrcyq8yt1uUYdfsVDabxRz?usp=drive_link' },
+        { title: 'Productions (PRODS)', link: 'https://drive.google.com/drive/folders/19xEJrWYceRueUfb2tMZoiX23WYG07McC?usp=drive_link' },
+        { title: 'Sales (SALE)', link: 'https://drive.google.com/drive/folders/1LcplWcstEOvq0Anli69ILa-s1VU77COZ?usp=drive_link' },
+        { title: 'Web Developer (WEB)', link: 'https://drive.google.com/drive/folders/1BI2v-tmlZPgS-K9B0fmFKT0DckdFNE5k?usp=drive_link' },
+        { title: 'Hackathon (HACK)', link: 'https://drive.google.com/drive/folders/1G5d6RKz5-xy5BTQ-fHDGbbh-cA0tof43?usp=drive_link' },
+        { title: 'Monthly Chair Meetings', link: 'https://drive.google.com/drive/folders/1p2W5XkpUDJdc0rHwdfJDbu0HD0y1ehdG?usp=sharing' }
     ],
-    external: [
-        { title: 'Careers and Professional Development (CAPD)', link: 'https://drive.google.com/drive/folders/1aCOtwl6-C7LnYfYqn5UwLAY8AGXrK9-N' },
-        { title: 'Community Engagement (COMME)', link: 'https://drive.google.com/drive/folders/1eAU4FpLzgoL7CjbNYNwaIqdal85h6qxI' },
-        { title: 'Sponsorship (SPSR)', link: 'https://drive.google.com/drive/folders/11Ro-DKKInsmtZpfzLlYREWnMQxkSbzxP' },
-        { title: 'Science Student Recognition Award Night (SSRAN)', link: 'https://drive.google.com/drive/folders/10vGkIVnb4vC3qNZ1bk4c2beumnNS-dhm' }
+    External: [
+        { title: 'Careers and Professional Development (CAPD)', link: 'https://drive.google.com/drive/folders/1aCOtwl6-C7LnYfYqn5UwLAY8AGXrK9-N?usp=drive_link' },
+        { title: 'Community Engagement (COMME)', link: 'https://drive.google.com/drive/folders/1eAU4FpLzgoL7CjbNYNwaIqdal85h6qxI?usp=drive_link' },
+        { title: 'Sponsorship (SPSR)', link: 'https://drive.google.com/drive/folders/11Ro-DKKInsmtZpfzLlYREWnMQxkSbzxP?usp=drive_link' },
+        { title: 'Science Student Recognition Award Night (SSRAN)', link: 'https://drive.google.com/drive/folders/10vGkIVnb4vC3qNZ1bk4c2beumnNS-dhm?usp=drive_link' }
     ],
-    finance: [
-        { title: 'Finance', link: 'https://drive.google.com/drive/folders/1LT6arf73onq1gkb6AQ7DNxXU7ANmmabd' },
-        { title: 'Grants', link: 'https://drive.google.com/drive/folders/1xk0eNmW0DnVa2UtYtzx0Rpd73R0e1FJ-' }
+    Finance: [
+        { title: 'Finance', link: 'https://drive.google.com/drive/folders/1LT6arf73onq1gkb6AQ7DNxXU7ANmmabd?usp=drive_link' },
+        { title: 'Grants', link: 'https://drive.google.com/drive/folders/1xk0eNmW0DnVa2UtYtzx0Rpd73R0e1FJ-?usp=drive_link' }
     ],
-    'student life': [
-        { title: 'First Year Committee (FYC)', link: 'https://drive.google.com/drive/folders/1zHz9y0guoXjDGGl_y8wJOMuYtX1yn5k4' },
-        { title: 'Flagship Experience (FLEX)', link: 'https://drive.google.com/drive/folders/1IMGF8f0fmIWhgThPdLhHqXKOvFN-8Uwf' },
-        { title: 'Social', link: 'https://drive.google.com/drive/folders/1Yaev7HKCXH9zOrMDg88Y3M-rGOd-SkOh' },
-        { title: 'Sports', link: 'https://drive.google.com/drive/folders/1ZAFdbO30q7dJcDgVH3U_4Ca7kyAmEU46' },
-        { title: 'Science Celebration Committee', link: 'https://drive.google.com/drive/folders/1o_eTQTh4jBmP1STxMf8c7qdHOkjCpK8S' },
+    'Student Life': [
+        { title: 'First Year Committee (FYC)', link: 'https://drive.google.com/drive/folders/1zHz9y0guoXjDGGl_y8wJOMuYtX1yn5k4?usp=drive_link' },
+        { title: 'Flagship Experience (FLEX)', link: 'https://drive.google.com/drive/folders/1IMGF8f0fmIWhgThPdLhHqXKOvFN-8Uwf?usp=drive_link' },
+        { title: 'Social', link: 'https://drive.google.com/drive/folders/1Yaev7HKCXH9zOrMDg88Y3M-rGOd-SkOh?usp=drive_link' },
+        { title: 'Sports', link: 'https://drive.google.com/drive/folders/1ZAFdbO30q7dJcDgVH3U_4Ca7kyAmEU46?usp=drive_link' },
+        { title: 'Science Celebration Committee', link: 'https://drive.google.com/drive/folders/1o_eTQTh4jBmP1STxMf8c7qdHOkjCpK8S?usp=drive_link' },
     ]
 }
