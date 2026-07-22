bca0b46c7cd603409421cbb348ae0d61b7172173
dracofs <dracofs.777@gmail.com>
2026-07-22 00:58:19 -0300

    updated event contact info

--- frontend/src/app/science-student-centre/event-bookings/page.tsx
+++ frontend/src/app/science-student-centre/event-bookings/page.tsx
@@ -376,8 +376,8 @@ export default function EventBookings() {
             {/* Contact Section */}
             <section className={styles.contact}>
                 <h2>Contact our ALSSC Team</h2>
-                <p>For Booking Inquiries: <a href="mailto:lmanager.bookings@ubcsus.ca">lmanager.bookings@ubcsus.ca</a></p>
-                <p>For Operations & Maintenance: <a href="mailto:lmanager.operations@ubcsus.ca">lmanager.operations@ubcsus.ca</a></p>
+                <p>For Booking Inquiries: <a href="mailto:bmanager.bookings@sus.ubc.ca">bmanager.bookings@sus.ubc.ca</a></p>
+                <p>For Operations & Maintenance: <a href="mailto:bmanager.operations@sus.ubc.ca">bmanager.operations@sus.ubc.ca</a></p>
             </section>
         </div>
     )
